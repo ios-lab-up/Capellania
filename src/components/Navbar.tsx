69aad93cef0df15351ebbdac5cbc59d894705f72
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-07 12:14:14 -0600

    added layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-navbar-color p-4 rounded-b-lg shadow-md">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <a href="/" className="text-2xl font-bold text-gray-800">
+          Capellanía UP
+        </a>
+        <div className="space-x-4">
+          <a href="/" className="text-gray-800 hover:text-white hover:bg-gray-600 transition-colors px-4 py-2 rounded-md">
+            Inicio
+          </a>
+          <a href="/about" className="text-gray-800 hover:text-white hover:bg-gray-600 transition-colors px-4 py-2 rounded-md">
+            Sobre Nosotros
+          </a>
+          <a href="/directory" className="text-gray-800 hover:text-white hover:bg-gray-600 transition-colors px-4 py-2 rounded-md">
+            Directorio
+          </a>
+          <a href="/contact" className="text-gray-800 hover:text-white hover:bg-gray-600 transition-colors px-4 py-2 rounded-md">
+            Contacto
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/mycomponent.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/MyComponent.jsx
-import React from 'react';
-
-const MyComponent = () => {
-  return <div>Hello from React Component!</div>;
-};
-
-export default MyComponent;
