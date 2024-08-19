1f69f7d5dbc5f6677212d246ec44c2a8b896f8ea
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-18 21:37:16 -0600

    eventos

--- Capellania_UP/CapellaniaUP/src/pages/public/ViewNotices.tsx
+++ Capellania_UP/CapellaniaUP/src/pages/public/ViewNotices.tsx
@@ -1,4 +1,3 @@
-// src/pages/public/ViewNotices.tsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -12,31 +11,31 @@ const ViewNotices: React.FC = () => {
   const [notices, setNotices] = useState<Notice[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/notices', {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-    .then(response => {
-      setNotices(response.data);
-    })
-    .catch(error => {
-      console.error("Error al obtener los avisos:", error);
-    });
+    axios.get('http://localhost:5000/api/notices')
+      .then(response => {
+        setNotices(response.data);
+      })
+      .catch(error => {
+        console.error("Error al obtener los avisos:", error);
+      });
   }, []);
 
   return (
-    <div className="min-h-screen bg-[#F0D8BE] flex flex-col items-center justify-center p-8">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-2xl w-full">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Avisos Recientes</h1>
-        <ul className="space-y-4">
-          {notices.map(notice => (
-            <li key={notice.id} className="p-4 border border-gray-200 rounded-lg bg-[#F9F9F9] shadow">
-              <p className="text-xl font-semibold text-gray-800">Título: {notice.title}</p>
-              <p className="text-lg text-gray-700">{notice.content}</p>
-            </li>
-          ))}
-        </ul>
+    <div className="min-h-screen bg-gradient-to-b from-[#F9EFE4] to-[#F0D8BE] flex flex-col items-center justify-center p-8">
+      <div className="container mx-auto p-8">
+        <h1 className="text-5xl font-bold text-gray-800 mb-8 text-center">Avisos Recientes</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {notices.length > 0 ? (
+            notices.map(notice => (
+              <div key={notice.id} className="bg-white shadow-lg rounded-lg p-6 transform hover:scale-105 transition">
+                <h3 className="text-xl font-semibold text-gray-800 mb-2">{notice.title}</h3>
+                <p className="text-gray-600 mt-4">{notice.content}</p>
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-700 text-center col-span-full">No hay avisos disponibles.</p>
+          )}
+        </div>
       </div>
     </div>
   );
